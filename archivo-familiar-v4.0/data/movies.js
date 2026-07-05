cd88f9924e969b41bc486a1f8ed95ce3291efc43
mateolarcade <mateolarcade@gmail.com>
2026-07-05 19:59:23 -0300

    Update movies.js

--- archivo-familiar-v4.0/data/movies.js
+++ archivo-familiar-v4.0/data/movies.js
@@ -114,7 +114,7 @@ window.MOVIES = [
     location: "Bella Vista",
     people: ["Fátima", "Mateo"],
     poster: "assets/posters/pecas-flequillos.png",
-    drivePreviewUrl: "https://drive.google.com/file/d/1eZ1egtg6_MET5A62o_npBTDwffBvRmcF/preview"
+    drivePreviewUrl: ""
   },
   {
     id: "papa-uritorco",
